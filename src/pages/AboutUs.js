27c11fc2f5aa4d312b4ad291b57f443d18379b85
nhidayahj <hidayah.joha@gmail.com>
2021-05-07 10:06:14 +0000

    fine-tuned responsiveness

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -14,7 +14,7 @@ export default function AboutUs() {
                     </div>
                     <div className="info-desc-one">
                         <div className="info-text">
-                            <p>We are a group of young and passionate individuals who loves yoga and meditation 
+                            <p className="info">We are a group of young and passionate individuals who loves yoga and meditation 
                                We work alongside professionals in aramotherapy
                                 to give you all the specially scented essential oils. Most of our diffusers are handcrafted 
                                 from artisans all around the the world, to give you that unique touch.
@@ -23,33 +23,33 @@ export default function AboutUs() {
                     </div>
                     
                 </div>
-                {/* <div className="info-two">
+                <div className="info-two">
                     <div className="info-img-two">
                         <img src={diffuser} alt="diffuser-info" className="img-two" />
                     </div>
                     <div className="info-desc-two">
                         <div className="info-text">
-                            <p>Most of our diffusers are specially handcrafted by talented 
+                            <p className="info">Most of our diffusers are specially handcrafted by talented 
                                 artisans, each representating a unique look. Perfect for your 
                                 decorated home items. 
                         </p>
                         </div>
                     </div>
-                </div> */}
-                {/* <div className="info-three">
+                </div>
+                <div className="info-three">
                     <div className="info-img-three">
                         <img src={oil} alt="oil-info" className="img-three" />
                     </div>
                     <div className="info-desc-three">
                         <div className="info-text">
-                            <p> Misty's essential oil are carefully selected to give you the calmness and relaxation, 
+                            <p className="info"> Misty's essential oil are carefully selected to give you the calmness and relaxation, 
                                 enhancing your spiritual
                                 practice, or use it for your meditaton. Apply to your wrists, feet and behind the ears.
 
                         </p>
                         </div>
                     </div>
-                </div> */}
+                </div>
 
             </div>
         </React.Fragment>
