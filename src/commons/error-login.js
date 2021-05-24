a3b1e0c71c3b5fdc0a90201668c13b6e52f060fd
Hidayah <hidayah.joha@gmail.com>
2021-05-24 12:57:04 +0800

    add back to login page

--- src/commons/error-login.js
+++ src/commons/error-login.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { Alert } from 'reactstrap'
+import React from "react";
+import { Alert } from "reactstrap";
+import { Link } from "react-router-dom";
 
 export default function ErrorLogin() {
-    {
-        return (
-            <React.Fragment>
-                <div className="container mt-4">
-                    <Alert color="danger">
-                        <p>You have entered invalid login credentials. Please try again.</p>
-                </Alert>
-                </div>
-            </React.Fragment>
-        )
-    } 
+  {
+    return (
+      <React.Fragment>
+        <div className="container mt-4">
+          <Alert color="danger">
+            <p>
+              You have entered invalid login credentials. Please try again. Back
+              to <Link to="/login">Login Page</Link>
+            </p>
+          </Alert>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
