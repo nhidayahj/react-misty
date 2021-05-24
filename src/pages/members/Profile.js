4223df353ed6fa2b839b0eeece0b0a1e4815aa68
Hidayah <hidayah.joha@gmail.com>
2021-05-24 13:11:50 +0800

    edit minor changes

--- src/pages/members/Profile.js
+++ src/pages/members/Profile.js
@@ -1,129 +1,164 @@
-import React, { useState, useEffect } from 'react';
-import config from '../../config'
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import config from "../../config";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
-import { Button, Alert } from 'reactstrap';
+import { Button, Alert } from "reactstrap";
 
 const baseUrl = config.baseUrl;
 
 export default function MemberProfile() {
+  const [profile, setProfile] = useState({});
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoaded, setLoaded] = useState(false);
+  // const [custName, setCustName] = useState({
+  //     'name': '',
+  //     'email': '',
+  //     'mobile': ''
+  // })
 
+  const history = useHistory();
 
-    const [profile, setProfile] = useState({});
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    const [isLoaded, setLoaded] = useState(false);
-    // const [custName, setCustName] = useState({
-    //     'name': '',
-    //     'email': '',
-    //     'mobile': ''
-    // })
+  useEffect(() => {
+    if (localStorage.getItem("customer_id") !== null) {
+      setLoggedIn(true);
+      setTimeout(() => {
+        const fetch = async () => {
+          const customer = await axios.get(baseUrl + "/api/members/profile", {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("accessToken"),
+            },
+          });
+          console.log(customer.data);
+          setProfile(customer.data);
+        };
+        fetch();
+      }, 1000);
+    }
+  }, []);
 
-    const history = useHistory();
+  const goCart = () => {
+    history.push(`/profile/cart`);
+  };
 
-    useEffect(() => {
-        if (localStorage.getItem('customer_id') !== null) {
-            setLoggedIn(true);
-            setTimeout(() => {
-                const fetch = async () => {
-                    const customer = await axios.get(baseUrl + '/api/members/profile', {
-                        'headers': {
-                            'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
-                        }
-                    })
-                    console.log(customer.data);
-                    setProfile(customer.data);
-                }
-                fetch();
-            }, 1000)
+  const goCatalog = () => {
+    history.push(`/diffusers`);
+  };
 
+  const updateProfile = async (e) => {
+    try {
+      let updateInfo = await axios.post(
+        `${baseUrl}/api/members/info/${e.target.name}/update`,
+        {
+          name: profile.name,
+          email: profile.email,
+          mobile_no: profile.mobile_no,
         }
-    }, [])
-
-    const goCart = () => {
-        history.push(`/profile/cart`)
+      );
+      validUpdate();
+    } catch (e) {
+      <Alert color="warning">
+        <p>Error submitting upate. Please try again. </p>
+      </Alert>;
     }
+  };
 
-    const updateProfile = async (e) => {
-        try {
-            let updateInfo = await axios.post(`${baseUrl}/api/members/info/${e.target.name}/update`, {
-                name: profile.name,
-                email: profile.email,
-                mobile_no: profile.mobile_no
-            });
-            validUpdate()
-        } catch (e) {
-            <Alert color="warning">
-                <p>Error submitting upate. Please try again. </p>
-            </Alert>
-        }
-    }
+  function validUpdate() {
+    return (
+      <React.Fragment>
+        <Alert color="success">
+          <p>Customer details updated</p>
+        </Alert>
+      </React.Fragment>
+    );
+  }
 
-    function validUpdate() {
-        return (
-            <React.Fragment>
-                <Alert color="success">
-                    <p>Customer details updated</p>
-                </Alert>
-            </React.Fragment>
-        )
-    }
+  const userLogout = async () => {
+    let logout = await axios.post(`${baseUrl}/api/members/logout`, {
+      refreshToken: localStorage.getItem("refreshToken"),
+    });
+    localStorage.clear();
+    console.log(logout.data.message);
+    history.push("/logout");
+  };
 
-    const userLogout = async () => {
-        let logout = await axios.post(`${baseUrl}/api/members/logout`, {
-            refreshToken: localStorage.getItem('refreshToken')
-        })
-        localStorage.clear();
-        console.log(logout.data.message);
-        history.push('/logout');
-    }
+  const updateName = (e) => {
+    let cloned = { ...profile };
+    cloned.name = e.target.value;
+    setProfile(cloned);
+  };
 
-    const updateName = (e) => {
-        let cloned = { ...profile };
-        cloned.name = e.target.value;
-        setProfile(cloned);
-    }
+  const updateEmail = (e) => {
+    let cloned = { ...profile };
+    cloned.email = e.target.value;
+    setProfile(cloned);
+  };
 
-    const updateEmail = (e) => {
-        let cloned = { ...profile };
-        cloned.email = e.target.value;
-        setProfile(cloned);
-    }
+  const updateMobile = (e) => {
+    let cloned = { ...profile };
+    cloned.mobile_no = e.target.value;
+    setProfile(cloned);
+  };
 
-    const updateMobile = (e) => {
-        let cloned = { ...profile };
-        cloned.mobile_no = e.target.value;
-        setProfile(cloned);
-    }
-
-    return <React.Fragment>
-
-        <div className="container profile-box">
-
-            <h3 className="product-headers mt-3">User Account</h3>
-            <div className="form-group">
-                <label className="form-label-text">Full Name</label>
-                <input type="text" className="form-control"
-                    name="name" value={profile.name} onChange={updateName} />
-            </div>
-            <div className="form-group">
-                <label>Email</label>
-                <input type="text" className="form-control"
-                    name="email" value={profile.email} onChange={updateEmail} />
-            </div>
-            <div className="form-group">
-                <label>Mobile No.</label>
-                <input type="text" className="form-control"
-                    name="mobile_no" value={profile.mobile_no} onChange={updateMobile} />
-            </div>
-
-            <Button color="info" className="profile-update mt-3"
-                onClick={updateProfile} name={localStorage.getItem('customer_id')}>Update Profile</Button>{' '}
-            <Button color="success" className="mt-3 profile-cart" onClick={goCart}>Shopping Cart</Button>
+  return (
+    <React.Fragment>
+      <div className="container profile-box">
+        <h3 className="product-headers mt-3">User Account</h3>
+        <div className="form-group">
+          <label className="form-label-text">Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={profile.name}
+            onChange={updateName}
+          />
         </div>
-            <div className="logout-btn mt-3 mb-4">
-                <Button color="danger" size="sm" className="profile-logout" onClick={userLogout}>Logout</Button>
-            </div>
-
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="text"
+            className="form-control"
+            name="email"
+            value={profile.email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className="form-group">
+          <label>Mobile No.</label>
+          <input
+            type="text"
+            className="form-control"
+            name="mobile_no"
+            value={profile.mobile_no}
+            onChange={updateMobile}
+          />
+        </div>
+        <Button
+          color="info"
+          className="profile-update mt-3"
+          onClick={updateProfile}
+          name={localStorage.getItem("customer_id")}
+        >
+          Update Profile
+        </Button>{" "}
+        <Button color="success" className="mt-3 profile-cart" onClick={goCart}>
+          Shopping Cart
+        </Button>{" "}
+        <Button color="secondary" className="mt-3 catalog" onClick={goCatalog}>
+          Browse Catalog
+        </Button>
+      </div>
+      <div className="logout-btn mt-3 mb-4">
+        <Button
+          color="danger"
+          size="sm"
+          className="profile-logout"
+          onClick={userLogout}
+        >
+          Logout
+        </Button>
+      </div>
     </React.Fragment>
+  );
 }
--- src/pages/products/Diffusers.js
+++ src/pages/products/Diffusers.js
@@ -1,195 +1,223 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import config from '../../config';
-import {Badge} from 'reactstrap';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import config from "../../config";
+import { Badge } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
-import cart from '../../icons/cart.png';
+import cart from "../../icons/cart.png";
 
-const baseUrl = config.baseUrl
+const baseUrl = config.baseUrl;
 
 export default function Diffusers() {
-    const history = useHistory();
+  const history = useHistory();
 
-    const [isLoaded, setLoaded] = useState(false);
-    const [diffusers, setDiffusers] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+  const [diffusers, setDiffusers] = useState([]);
 
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    // const [customerId, setCustomerId] = useState('');
-    // const [addDiffuserCart, setDiffuserCart] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  // const [customerId, setCustomerId] = useState('');
+  // const [addDiffuserCart, setDiffuserCart] = useState(false);
 
-    // const [searchDiffName, setDiffName] = useState('');
-    const [searchCategory, setCategory] = useState('');
-    const [searchPriceRange, setPrice] = useState('');
-    const [categoryInfo, setCategoryInfo] = useState('');
+  // const [searchDiffName, setDiffName] = useState('');
+  const [searchCategory, setCategory] = useState("");
+  const [searchPriceRange, setPrice] = useState("");
+  const [categoryInfo, setCategoryInfo] = useState("");
 
-
-    useEffect(() => {
-        setTimeout(() => {
-            const fetch = async () => {
-                let diffusers = await axios.get(`${baseUrl}/api/products/diffusers`);
-                setDiffusers(diffusers.data);
-                // console.log("All diffusers: ", diffusers.data);
-                setLoaded(true);
-                if (localStorage.getItem('customer_id') !== null) {
-                    setLoggedIn(true);
-                }
-            }
-            fetch();
-        }, 500)
-    }, []);
-
-    // search products by categories
-    useEffect(() => {
-        setTimeout(() => {
-            const fetch = async () => {
-                // let category = searchCategory;
-                if (searchCategory !== '0') {
-                    const response = await axios.get(`${baseUrl}/api/products/diffuser/category/${searchCategory}`);
-                    console.log(response.data);
-                    setDiffusers(response.data);
-                    setCategoryInfo(response.data.category)
-                } else {
-                    const response = await axios.get(`${baseUrl}/api/products/diffusers`);
-                    setDiffusers(response.data);
-                }
-            }
-            fetch();
-        }, 500)
-
-    }, [searchCategory])
-
-    // // search products by price range
-    useEffect(() => {
-        setTimeout(() => {
-            // let range = searchPriceRange;
-            const fetch = async () => {
-                if (searchPriceRange === '3499' || searchPriceRange === '6500') {
-                    const responseLow = await axios.get(`${baseUrl}/api/products/diffuser/low/${searchPriceRange}`);
-                    console.log("Low: ", responseLow.data);
-                    setDiffusers(responseLow.data);
-
-                } else if (searchPriceRange === '6501' || searchPriceRange === '10000') {
-                    const responseMid = await axios.get(`${baseUrl}/api/products/diffuser/mid/${searchPriceRange}`);
-                    console.log("Mid: ", responseMid.data);
-                    setDiffusers(responseMid.data);
-                } else if (searchPriceRange === '0') {
-                    const response = await axios.get(`${baseUrl}/api/products/diffusers`);
-                    setDiffusers(response.data);
-                }
-            }
-            fetch()
-        }, 500)
-    }, [searchPriceRange])
-
-    const diffAddToCart = async (e) => {
-        if (isLoggedIn === true && isLoaded === true) {
-            console.log("OK")
-            console.log("id:", e.currentTarget.name);
-            let product_id = e.currentTarget.name;
-            let customer_id = localStorage.getItem('customer_id');
-            let diffuser = await axios.get(`${baseUrl}/api/shoppingCart/diffuser/${customer_id}/${product_id}/addtocart`);
-            console.log(diffuser.data);
-            
-            if (diffuser.status == 200) {
-                alert("Item added into cart")
-            } else if (diffuser.status !== 200){
-                alert("Item not added")
-            }
+  useEffect(() => {
+    setTimeout(() => {
+      const fetch = async () => {
+        let diffusers = await axios.get(`${baseUrl}/api/products/diffusers`);
+        setDiffusers(diffusers.data);
+        // console.log("All diffusers: ", diffusers.data);
+        setLoaded(true);
+        if (localStorage.getItem("customer_id") !== null) {
+          setLoggedIn(true);
         }
+      };
+      fetch();
+    }, 500);
+  }, []);
 
-        if (isLoggedIn === false) {
-            history.push('/login', {
-                message: "Please login to continue shopping!"
-            });
+  // search products by categories
+  useEffect(() => {
+    setTimeout(() => {
+      const fetch = async () => {
+        // let category = searchCategory;
+        if (searchCategory !== "0") {
+          const response = await axios.get(
+            `${baseUrl}/api/products/diffuser/category/${searchCategory}`
+          );
+          console.log(response.data);
+          setDiffusers(response.data);
+          setCategoryInfo(response.data.category);
+        } else {
+          const response = await axios.get(`${baseUrl}/api/products/diffusers`);
+          setDiffusers(response.data);
         }
+      };
+      fetch();
+    }, 500);
+  }, [searchCategory]);
+
+  // // search products by price range
+  useEffect(() => {
+    setTimeout(() => {
+      // let range = searchPriceRange;
+      const fetch = async () => {
+        if (searchPriceRange === "3499" || searchPriceRange === "6500") {
+          const responseLow = await axios.get(
+            `${baseUrl}/api/products/diffuser/low/${searchPriceRange}`
+          );
+          console.log("Low: ", responseLow.data);
+          setDiffusers(responseLow.data);
+        } else if (
+          searchPriceRange === "6501" ||
+          searchPriceRange === "10000"
+        ) {
+          const responseMid = await axios.get(
+            `${baseUrl}/api/products/diffuser/mid/${searchPriceRange}`
+          );
+          console.log("Mid: ", responseMid.data);
+          setDiffusers(responseMid.data);
+        } else if (searchPriceRange === "0") {
+          const response = await axios.get(`${baseUrl}/api/products/diffusers`);
+          setDiffusers(response.data);
+        }
+      };
+      fetch();
+    }, 500);
+  }, [searchPriceRange]);
+
+  const diffAddToCart = async (e) => {
+    if (isLoggedIn === true && isLoaded === true) {
+      console.log("OK");
+      console.log("id:", e.currentTarget.name);
+      let product_id = e.currentTarget.name;
+      let customer_id = localStorage.getItem("customer_id");
+      let diffuser = await axios.get(
+        `${baseUrl}/api/shoppingCart/diffuser/${customer_id}/${product_id}/addtocart`
+      );
+      console.log(diffuser.data);
+
+      if (diffuser.status == 200) {
+        alert("Item added into cart");
+      } else if (diffuser.status !== 200) {
+        alert("Item not added");
+      }
     }
 
-    const displayDiffusers = () => {
-        if (diffusers && diffusers.length >= 1) {
-            let products = [];
-            console.log("All diffusers: ",diffusers)
-            for (let diffuser of diffusers) {
-                products.push(
-                    <React.Fragment>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="sampleCard">
-                                <div className="product-image">
-                                    <img src={diffuser.image_url} alt="product image" className="product-img" />
-                                </div>
-                                <div className="sampleInfo">
-                                    <h5 className="product-card-title"> {diffuser.diffuser_name}</h5>
-                                    <Badge pill className="product-cat">{diffuser.category.name}</Badge>
-                                    <div className="product-desc">{diffuser.description}</div>
-                                    <div className="product-price">{formatPrice(diffuser.cost)} SGD</div>
-                                    <button className="product-cart-btn"  name={diffuser.id}
-                                        onClick={diffAddToCart}><span><img src={cart} alt="cart-icon"/></span></button>
-
-                                </div>
-                            </div>
-                        </div>
-                    </React.Fragment>
-                )
-            }
-            return products;
-        }
-        else if (!diffusers || diffusers.length < 1) {
-            return (
-                <React.Fragment>
-                    <div className="container alert alert-danger">
-                        <p>Product not found</p>
-                    </div>
-                </React.Fragment>
-            )
-        }
+    if (isLoggedIn === false) {
+      history.push("/login", {
+        message: "Please login to continue shopping!",
+      });
     }
+  };
 
-    function formatPrice(price) {
-        return parseFloat(price / 100.00).toFixed(2)
-    }
-
-
-    return (
-        <React.Fragment>
-            <div className="container">
-
-                <h3 className="product-headers">Search Item</h3>
-
-                <div className="form-group">
-                    <label>Price Range</label>
-                    <select className="form-control" name={searchPriceRange}
-                        onChange={e => { setPrice(e.target.value) }}>
-                        <option value="0">All</option>
-                        <option value="3499"> Less than 35 SGD</option>
-                        <option value="6500">35 - 65</option>
-                        <option value="6501">> 65 SGD</option>
-                        <option value="10000">> 100 SGD</option>
-                    </select>
+  const displayDiffusers = () => {
+    if (diffusers && diffusers.length >= 1) {
+      let products = [];
+      console.log("All diffusers: ", diffusers);
+      for (let diffuser of diffusers) {
+        products.push(
+          <React.Fragment>
+            <div className="col-lg-4 col-md-6">
+              <div className="sampleCard">
+                <div className="product-image">
+                  <img
+                    src={diffuser.image_url}
+                    alt="product image"
+                    className="product-img"
+                  />
                 </div>
-
-                <div className="form-group">
-                    <label>Search by Category</label>
-                    <select className="form-control" name={searchCategory}
-                        onChange={e => { setCategory(e.target.value) }}>
-                        <option value="0">All</option>
-                        <option value="1">Reed</option>
-                        <option value="2">Nebulizer</option>
-                        <option value="3">Water</option>
-                        <option value="4">Candle</option>
-                        <option value="5">Electric</option>
-                    </select>
+                <div className="sampleInfo">
+                  <h5 className="product-card-title">
+                    {" "}
+                    {diffuser.diffuser_name}
+                  </h5>
+                  <Badge pill className="product-cat">
+                    {diffuser.category.name}
+                  </Badge>
+                  <div className="product-desc">{diffuser.description}</div>
+                  <div className="product-price">
+                    {formatPrice(diffuser.cost)} SGD
+                  </div>
+                  <button
+                    className="product-cart-btn"
+                    name={diffuser.id}
+                    onClick={diffAddToCart}
+                  >
+                    <span>
+                      <img src={cart} alt="cart-icon" />
+                    </span>
+                  </button>
                 </div>
-
-                <div className="page-banner">
-                    <h3 className="product-headers">
-                        Explore our Misty Diffusers</h3>
-                </div>
-                <div className="row mb-4">
-                    {displayDiffusers()}
-                </div>
-
+              </div>
             </div>
+          </React.Fragment>
+        );
+      }
+      return products;
+    } else if (!diffusers || diffusers.length < 1) {
+      return (
+        <React.Fragment>
+          <div className="container alert alert-danger">
+            <p>Product not found</p>
+          </div>
         </React.Fragment>
-    )
+      );
+    }
+  };
+
+  function formatPrice(price) {
+    return parseFloat(price / 100.0).toFixed(2);
+  }
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h3 className="product-headers">Search Item</h3>
+
+        <div className="form-group">
+          <label>Price Range</label>
+          <select
+            className="form-control"
+            name={searchPriceRange}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          >
+            <option value="0">All</option>
+            <option value="3499"> {"<"} 35 SGD</option>
+            <option value="6500">35 - 65</option>
+            <option value="6501">{">"} 65 SGD</option>
+            <option value="10000">{">"} 100 SGD</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label>Search by Category</label>
+          <select
+            className="form-control"
+            name={searchCategory}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+          >
+            <option value="0">All</option>
+            <option value="1">Reed</option>
+            <option value="2">Nebulizer</option>
+            <option value="3">Water</option>
+            <option value="4">Candle</option>
+            <option value="5">Electric</option>
+          </select>
+        </div>
+
+        <div className="page-banner">
+          <h3 className="product-headers">Explore our Misty Diffusers</h3>
+        </div>
+        <div className="row mb-4">{displayDiffusers()}</div>
+      </div>
+    </React.Fragment>
+  );
 }
